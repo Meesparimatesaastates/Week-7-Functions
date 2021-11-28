2f094c587a2424041ee1ecbc29a984c6d58bff45
Martin Ubei-Kon <mubeiko@tktk.ee>
2021-11-28 12:29:52 +0200

    Add project files.

---
+++ UserNameData/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace UserNameData
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //program küsib kasutajal sisestada oma eesnime
+            //programm kuvab kasutaja eesnime pikkust
+            //programm kuvab kasutaja eesnime esimest tähte
+            //program kuvab kasutaja eesnime tagurpidi
+            //main meetordis ei tohi olla rohkem kui kolm rida koodi
+
+            Console.WriteLine("Sisesta oma eesnimi: ");
+            string userName = (Console.ReadLine());
+            GetUserNameData(userName);  
+        }
+
+        public static void GetUserNameData(string userInput)
+        {
+            Console.WriteLine($"Sinu nimi on {userInput.Length} sümbolit pikk.");
+            Console.WriteLine($"Sinu eesnime esimene täht on {userInput[0]}");
+
+            for (int i = userInput.Length - 1; i >= 0; i--)
+            {
+                Console.Write(userInput[i]);
+            }
+
+        }
+    }
+}
---
+++ UserNameDataTask/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace UserNameDataTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutajal sisestada eesnime;
+            //programm küsib kasutajal sisestada numbrit 1-3;
+            //kui kasutaja sisestab '1', siis kuvatakse kasutaja eesnime tagurpidi;
+            //kui kasutaja valib '2', siis kuvatakse kasutaja eesnime esimest tähte;
+            //kui kasutaja valib '3', siis kuvatakse kasutaja esinime pikkust;
+
+            Console.WriteLine("Sisesta oma eesnimi: ");
+            string userName = (Console.ReadLine());
+            Console.WriteLine("sisesta arv 1 kuni 3'ni");
+            int UserNumber = Convert.ToInt32(Console.ReadLine());          
+
+            switch (UserNumber)
+            {
+                case 1:
+                    GetUserNameData1(userName);
+                    break;
+                case 2:
+                    GetUserNameData2(userName);
+                    break;
+                case 3:
+                    GetUserNameData3(userName);
+                    break;
+                default:
+                    Console.WriteLine("Kena päeva");
+                    break;
+            } 
+            
+        } public static void GetUserNameData1(string userInput)
+        {
+            for (int i = userInput.Length - 1; i >= 0; i--)
+            {
+                Console.Write(userInput[i]);
+            }
+        }   
+        public static void GetUserNameData2(string userInput)
+        {
+            Console.WriteLine($"Sinu eesnime esimene täht on {userInput[0]}"); 
+        }public static void GetUserNameData3(string userInput)
+        {
+            Console.WriteLine($"Sinu nimi on {userInput.Length} sümbolit pikk.");
+        }
+    }
+}
+
